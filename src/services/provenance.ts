255e9688e99552e2920e2231c3fdd3b3be431892
Visage <49914133+Visaging@users.noreply.github.com>
2026-09-13 22:40:48 +0530

    Delete src directory

--- src/services/provenance.ts
+++
@@ -1,6 +0,0 @@
-import type { ProvenanceNode } from '../types';
-import { appState } from './api';
-
-export async function getProvenanceNodes(experimentId: string): Promise<ProvenanceNode[]> {
-  return appState.getProvenanceNodes(experimentId);
-}
